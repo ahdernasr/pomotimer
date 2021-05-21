be613c3e2b33a2b130fb49cf9dec2acff6b940f4
ahdernasr <44983175+ahdernasr@users.noreply.github.com>
2021-05-21 01:33:02 +0100

    Updated timer pages

--- pomodoro/src/Pomo.js
+++ pomodoro/src/Pomo.js
@@ -1,48 +1,75 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useRef } from "react";
+
 const Pomo = (props) => {
   let TIME_LIMIT = 10;
   let timePassed = 0;
   let timeLeft = TIME_LIMIT;
   let timerContinue = true;
+  let color = "grey";
+  let baseTimerLabel;
+  let baseTimerPathRemaining;
+  let remainingPathColor;
 
-  const formatTimeLeft = (time) => {
-    // The largest round integer less than or equal to the result of time divided being by 60.
-    const minutes = Math.floor(time / 60);
+  useEffect(() => {
+    baseTimerLabel = useRef()
+    baseTimerPathRemaining = useRef()
+  }, [])
 
-    // Seconds are the remainder of the time divided by 60 (modulus operator)
-    let seconds = time % 60;
+  console.log(baseTimerLabel, baseTimerPathRemaining)
 
-    // If the value of seconds is less than 10, then display seconds with a leading zero
-    if (seconds < 10) {
-      seconds = `0${seconds}`;
-    }
+  // const formatTimeLeft = (time) => {
+  //   const minutes = Math.floor(time / 60);
+  //   let seconds = time % 60;
+  //   if (seconds < 10) {
+  //     seconds = `0${seconds}`;
+  //   }
+  //   return `${minutes}:${seconds}`;
+  // };
 
-    // The output in MM:SS format
-    return `${minutes}:${seconds}`;
-  };
+  // function startTimer() {
+  //   setInterval(() => {
+  //     if (timerContinue) {
+  //       timePassed = timePassed += 1;
+  //       timeLeft = TIME_LIMIT - timePassed;
+  //     } else {
+  //       color = "red";
+  //     }
+  //     if (timeLeft === 0) {
+  //       timerContinue = false;
+  //     }
+  //     baseTimerLabel.current.innerHTML = formatTimeLeft(timeLeft);
 
-  function startTimer() {
-    setInterval(() => {
-      // The amount of time passed increments by one
-      if (timerContinue) {
-        timePassed = timePassed += 1;
-        timeLeft = TIME_LIMIT - timePassed;
-      }
-      if (timeLeft === 0) {
-        timerContinue = false;
-      }
+  //     setCircleDasharray();
+  //   }, 1000);
+  // }
 
-      // The time left label is updated
-      document.getElementById("base-timer-label").innerHTML =
-        formatTimeLeft(timeLeft);
-    }, 1000);
-  }
+  // const COLOR_CODES = {
+  //   info: {
+  //     color: "green",
+  //   },
+  // };
 
-  startTimer();
+  // let remainingPathColor = COLOR_CODES.info.color;
+  // function calculateTimeFraction() {
+  //   const rawTimeFraction = timeLeft / TIME_LIMIT;
+  //   return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+  // }
 
+  // let FULL_DASH_ARRAY = 283;
+
+  // function setCircleDasharray() {
+  //   const circleDasharray = `${(
+  //     calculateTimeFraction() * FULL_DASH_ARRAY
+  //   ).toFixed(0)} 283`;
+  //   baseTimerPathRemaining.current.setAttribute("stroke-dasharray", circleDasharray);
+  // }
+
+  // startTimer();
   return (
     <div className="home">
       <div className="base">
-        <div className="base-content top">
+        <div id="timer-container" className="base-content top">
           <div className="base-timer">
             <svg
               className="base-timer__svg"
@@ -52,18 +79,37 @@ const Pomo = (props) => {
               <g className="base-timer__circle">
                 <circle
                   className="base-timer__path-elapsed"
+                  style={{stroke: color}}
                   cx="50"
                   cy="50"
                   r="45"
                 ></circle>
+                <path
+                  ref={baseTimerPathRemaining}
+                  id="base-timer-path-remaining"
+                  strokeDasharray="283"
+                  className={`base-timer__path-remaining ${remainingPathColor}`}
+                  d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+                ></path>
               </g>
             </svg>
-            <span id="base-timer-label" className="base-timer__label">
-              {formatTimeLeft(timeLeft)}
+            <span  id="base-timer-label" ref={baseTimerLabel} className="base-timer__label">
             </span>
           </div>
         </div>
-        <div className="base-content bottom"></div>
+        <div className="base-content bottom">
+          <h2 className="session-info">
+            <i className="fas fa-coffee"></i>: {props.data.break} min{" "}
+          </h2>
+          <h2 className="session-info">
+            <i className="fas fa-hashtag"></i>: {props.data.sessions} sessions{" "}
+          </h2>
+        </div>
       </div>
     </div>
   );
